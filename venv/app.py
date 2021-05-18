1eff1357e0721c89c43a495be410f4f09c3a0d65
memorygardens <brandonoleksik@gmail.com>
2021-05-17 22:08:12 -0400

    removed looping

--- venv/app.py
+++ venv/app.py
@@ -17,17 +17,10 @@ def home():
         query = wikipedia.search(search, results = 3)
         
         # Fetch data from wikipedia
-        #loop here. 
-        for x in query:
-            print(x)
-            page = wikipedia.page(x)
-            print(page)
         pages = wikipedia.page(query)
-        print("help")
-        print(pages[1].url)
         result = (pages.url)
-        print(result)
-        return f"<h2>{pages}</h2>"
+     
+        return f"<h2>{result}</h2>"
   
   
 if __name__ == '__main__':
