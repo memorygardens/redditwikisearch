c112841915b3d3a83a45fda996e400d0d4f496d1
memorygardens <brandonoleksik@gmail.com>
2021-05-18 01:17:31 -0400

    updated comments, and layout. gave reasoning for not using answers.html

--- venv/app.py
+++ venv/app.py
@@ -6,20 +6,25 @@ app = Flask(__name__)
 #app.config['SERVER_NAME']='.wiki-search.com'
 #app.url_map.default_subdomain = "*"
 
-# create HOME View
+
 @app.route("/", methods=['POST', 'GET'])
+#adding subdomain = "{search}" bricks it, but I know it is necessary to change * to the correct search term based on the users input. 
 def home():
+    #display index if no search is happening 
     if request.method == "GET":
         return render_template("index.html")
+    #display search form and begin using wikipedia api
     else:
         search = request.form["search"]
-
+        #search wiki based on search term 3 results currently
         query = wikipedia.search(search, results = 3)
         
-        # Fetch data from wikipedia
+        #get the wiki page is needed to get the url
         pages = wikipedia.page(query)
+        #get the url
         result = (pages.url)
-     
+
+        #pass url into a string and display to user
         return f"<h2>{result}</h2>"
   
   
